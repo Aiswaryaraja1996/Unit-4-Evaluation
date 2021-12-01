394c9c18c306ee7a6a18a21cb689634548d5d920
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2021-12-01 21:25:04 +0530

    5 Commit

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import axios from "axios";
+
+const addUserDetail = (userDetail) => {
+  const config = {
+    url: "http://localhost:3001/orders",
+    method: "POST",
+    data: userDetail,
+  };
+  return axios(config);
+};
+
 const CarCard = ({ data }) => {
+  const [isBuy, setBuy] = useState(false);
+  const [userDetail, setUserDetail] = useState({});
+
+  const handleInput = (e) =>
+    setUserDetail({ ...userDetail, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(userDetail);
+    addUserDetail(userDetail);
+    setBuy(false);
+    alert("Car Booked Successfully");
+  };
+
   return (
     <div>
       <div>
@@ -6,13 +33,24 @@ const CarCard = ({ data }) => {
         <img src={data.image} alt={data.name}></img>
       </div>
       <div>
-          <p>Price : Rs.{data.price}</p>
-          <p>Model : {data.type}</p>
-          <p>Year  : {data.year}</p>
+        <p>Price : Rs.{data.price}</p>
+        <p>Model : {data.type}</p>
+        <p>Year : {data.year}</p>
       </div>
       <div>
-          <button>BUY</button>
+        <button onClick={() => setBuy(true)}>BUY</button>
       </div>
+      {isBuy && (
+        <form onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input type="text" name="name" onChange={handleInput}></input>
+          <br />
+          <label>Phone</label>
+          <input type="text" name="phone" onChange={handleInput}></input>
+          <br />
+          <button>Submit</button>
+        </form>
+      )}
     </div>
   );
 };
